56302d0c0a033656ddc9d12918cd9c2112b10a83
Serena Fang <sfang97@gmail.com>
2019-03-05 05:31:53 -0600

    Create Interest.cpp
    Simple bank interest problem

---
+++ Interest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int prob1() {
+        float bank1 = 100;
+        float bank2 = 200;
+        int years = 1;
+        while ((bank2*(1.02*years)) > (bank1*(1.05*years))) {
+                bank1 = bank1*1.05;
+                bank2 = bank2*1.02;
+                years += 1;
+        }
+        bank1 *= 1.05;
+        bank2 *= 1.02;
+        cout << "Assuming interest gets compounded yearly, Account 1 will have $"  << bank1 << ", which will be more than Account 2, which will have $" << bank2 << ", in " << years << " years." <<  endl;
+
+}
